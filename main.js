675f9ec2b54486816cb7875415a2ef26b03d5622
ZenBakr <99152082+ZenBakr@users.noreply.github.com>
2022-12-16 16:02:07 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+function preload()
+{
+}
+
+function setup()
+{
+    canvas = createCanvas(450, 450);
+    canvas.center();
+}
+
+function draw()
+{
+}
+
+function picture()
+{
+    save('mylipstickFilterimg.png')
+}
